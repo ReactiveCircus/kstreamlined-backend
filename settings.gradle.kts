7f29c3f18a72e6379c8a0be4befcf50718008273
Yang <ychescale9@gmail.com>
2023-10-28 15:48:30 +1100

    Fix toolchain.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,36 @@ rootProject.name = "kstreamlined-backend"
 
 pluginManagement {
     repositories {
+        gradlePluginPortal {
+            content {
+                includeGroupByRegex("org.gradle.*")
+                includeGroupByRegex("com.google.cloud.tools.*")
+                includeGroup("com.netflix.dgs.codegen")
+            }
+        }
         mavenCentral()
-        gradlePluginPortal()
     }
+
+    val toolchainsResolverVersion = file("$rootDir/gradle/libs.versions.toml")
+        .readLines()
+        .first { it.contains("toolchainsResolver") }
+        .substringAfter("=")
+        .trim()
+        .removeSurrounding("\"")
+
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version toolchainsResolverVersion
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention")
 }
