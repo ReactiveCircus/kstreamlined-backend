a8583661faf472753bc748c764435a75a6e28880
Yang <ychescale9@gmail.com>
2025-09-22 20:52:06 +1000

    Build with JDK 25.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ graalvmNative {
     }
     binaries.configureEach {
         javaLauncher = javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(24))
+            languageVersion.set(JavaLanguageVersion.of(25))
             vendor.set(JvmVendorSpec.GRAAL_VM)
         }
         resources.autodetect()
